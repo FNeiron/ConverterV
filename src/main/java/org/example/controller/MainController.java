e25aa6842023006bba9e8d19d549d3bd11a88899
FNeiron <fazylov.2002@mail.ru>
2023-07-11 02:34:47 +0400

    added history of user's messages

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -3,7 +3,6 @@ package org.example.controller;
 import org.example.domain.Message;
 import org.example.domain.User;
 import org.example.repos.MessageRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,25 +10,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 public class MainController {
-    @Autowired
-    private MessageRepo messageRepo;
+    private final MessageRepo messageRepo;
+
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String greeting() {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(@RequestParam(required = false) String filter, Model model) {
+    public String general(
+            @AuthenticationPrincipal User user,
+            @RequestParam(required = false) String filter, Model model) {
         Iterable<Message> messages;
 
         if(filter != null && !filter.isEmpty())
-            messages = messageRepo.findByTag(filter);
+            messages = messageRepo.findByTagAndAuthor(filter, user);
         else
-            messages = messageRepo.findAll();
+            messages = messageRepo.findByAuthor(user);
         model.addAttribute("messages", messages);
 
         model.addAttribute("filter", filter);
@@ -43,7 +45,7 @@ public class MainController {
         Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
-        Iterable<Message> messages = messageRepo.findAll();
+        Iterable<Message> messages = messageRepo.findByAuthor(user);
         model.addAttribute("messages", messages);
         return "main";
     }
--- src/main/java/org/example/repos/MessageRepo.java
+++ src/main/java/org/example/repos/MessageRepo.java
@@ -1,6 +1,7 @@
 package org.example.repos;
 
 import org.example.domain.Message;
+import org.example.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Long> {
     List<Message> findByTag(String tag);
+    List<Message> findByAuthor(User user);
+    List<Message> findByTagAndAuthor(String tag, User user);
 }
