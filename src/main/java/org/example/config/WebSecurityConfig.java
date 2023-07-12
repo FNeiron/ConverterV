f2627629d6f08fdd5a89b871236e61c96e73a682
FNeiron <fazylov.2002@mail.ru>
2023-07-12 14:54:02 +0400

    Added error and 404 pages

--- src/main/java/org/example/config/WebSecurityConfig.java
+++ src/main/java/org/example/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/registration").permitAll()
+                        .requestMatchers("/", "/registration", "/error", "/404").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
